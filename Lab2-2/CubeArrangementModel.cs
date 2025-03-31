e537546d60e012b1f528f3e216339965dfab30c5
Janka Pásztor-Szőke <pasztorszokejanka@gmail.com>
2025-03-31 20:29:20 +0300

    AdvanceTime changes

--- Lab2-2/CubeArrangementModel.cs
+++ Lab2-2/CubeArrangementModel.cs
@@ -18,20 +18,11 @@ namespace Szeminarium
         /// </summary>
         private double Time { get; set; } = 0;
 
-        /// <summary>
-        /// The value by which the center cube is scaled. It varies between 0.8 and 1.2 with respect to the original size.
-        /// </summary>
-        public double CenterCubeScale { get; private set; } = 1;
 
-        /// <summary>
-        /// The angle with which the diamond cube is rotated around the diagonal from bottom right front to top left back.
-        /// </summary>
-        public double DiamondCubeLocalAngle { get; private set; } = 0;
+        public float rotationX { get; set; } = 0;
 
-        /// <summary>
-        /// The angle with which the diamond cube is rotated around the global Y axes.
-        /// </summary>
-        public double DiamondCubeGlobalYAngle { get; private set; } = 0;
+        private float stopper = 0;
+
 
         internal void AdvanceTime(double deltaTime)
         {
@@ -42,13 +33,14 @@ namespace Szeminarium
             // set a simulation time
             Time += deltaTime;
 
-            // lets produce an oscillating scale in time
-            CenterCubeScale = 1 + 0.2 * Math.Sin(1.5 * Time);
+            rotationX = (float)(Time * Math.PI / 2f);
+            stopper = (float)(Time * Math.PI / 2f);
 
-            // the rotation angle is time x angular velocity;
-            DiamondCubeLocalAngle = Time * 10;
-
-            DiamondCubeGlobalYAngle = -Time;
+            if (stopper > Math.PI / 2f) // ha > 90 fok
+            {
+                AnimationEnabled = false;
+                stopper = 0;
+            }
         }
     }
 }
